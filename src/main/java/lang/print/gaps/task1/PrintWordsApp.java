90d4e2236fb25f9b098e6006a1cf49ebe0c70216
yuldashevmuhiddin65 <102411263+yuldashevmuhiddin65@users.noreply.github.com>
2023-05-20 02:04:00 +0500

    I`ve made it!

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -3,6 +3,6 @@ package lang.print.gaps.task1;
 public class PrintWordsApp {
     public static void main(String[] args) {
         //write your code here
-        System.out.println("Java \n is \n great\n language");
+        System.out.println("java\nis\nsmth");
     }
 }
